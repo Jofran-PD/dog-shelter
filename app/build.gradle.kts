fa424c69d2d7ee2ce161e4f9ebd5f3e52eb746b5
Jofran-PD <eng.jofran.duran@gmail.com>
2026-03-27 15:24:00 -0400

    Add Hilt and KSP dependencies and plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.android.hilt)
+    alias(libs.plugins.devtools.ksp)
 }
 
 android {
@@ -51,6 +53,9 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.android.hilt)
+    implementation(libs.androidx.hilt.navigation.compose)
+    ksp(libs.android.hilt.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
