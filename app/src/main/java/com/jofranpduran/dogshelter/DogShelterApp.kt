de5026ed485b38c364445fbbffccaba4f83cc950
Jofran-PD <eng.jofran.duran@gmail.com>
2026-03-27 16:15:04 -0400

    Setup Hilt in Application and MainActivity

---
+++ app/src/main/java/com/jofranpduran/dogshelter/DogShelterApp.kt
@@ -0,0 +1,7 @@
+package com.jofranpduran.dogshelter
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DogShelterApp: Application()
--- app/src/main/java/com/jofranpduran/dogshelter/ui/MainActivity.kt
+++ app/src/main/java/com/jofranpduran/dogshelter/ui/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.jofranpduran.dogshelter.ui.theme.DogShelterTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
